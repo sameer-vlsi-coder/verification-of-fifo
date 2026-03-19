f335e5d2c4d87fd11766f53346e9a37f347bd18f
md sofi sameer <mdsofisameersameer@gmail.com>
2026-03-20 01:25:31 +0530

    Create design.sv

---
+++ design.sv
@@ -0,0 +1,52 @@
+DESIGN CODE:
+
+module FIFO(input clk, rst, wr, rd,
+            input [7:0] data_in, output reg [7:0] data_out,
+            output empty, full);
+  
+  reg [3:0] wptr = 0, rptr = 0;
+  
+  reg [4:0] cnt = 0;
+  
+  reg [7:0] mem [15:0];
+ 
+  always @(posedge clk)
+    begin
+      if (rst == 1'b1)
+        begin
+          wptr <= 0;
+          rptr <= 0;
+          cnt  <= 0;
+        end
+      else if (wr && !full)
+        begin
+          mem[wptr] <= data_in;
+          wptr      <= wptr + 1;
+          cnt       <= cnt + 1;
+        end
+      else if (rd && !empty)
+        begin
+         
+          data_out <= mem[rptr];
+          rptr <= rptr + 1;
+          cnt  <= cnt - 1;
+        end
+    end
+ 
+ 
+  assign empty = (cnt == 0) ? 1'b1 : 1'b0;
+  assign full  = (cnt == 16) ? 1'b1 : 1'b0;
+ 
+endmodule
+ 
+//////////////////////////////////////
+
+interface fifo_if;
+  
+  logic clk, rd, wr;         
+  logic full, empty;          
+  logic [7:0] data_in;         
+  logic [7:0] data_out;        
+  logic rst;                  
+ 
+endinterface
